e84a53e71970092d88e19302a640146eece8832e
Dharmik Chhatbar <dharmikchhatbar01@gmail.com>
2025-01-08 18:50:23 -0600

    Initial Spring Boot Project Setup

---
+++ src/test/java/com/dharmik/finance/personalfinancetracker/PersonalFinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dharmik.finance.personalfinancetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
